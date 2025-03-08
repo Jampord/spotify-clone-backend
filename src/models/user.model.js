e51b30c50436f06c4a8437aaa26e76b0bc20c9f3
John Ford Actub <cs.actubjohnford@gmail.com>
2025-03-08 11:48:18 +0800

    created user model

---
+++ src/models/user.model.js
@@ -0,0 +1,22 @@
+//Creating User Model using Mongoose
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {timestamps: true} // for createdAt or updatedAt 
+);
+
+export const User = mongoose.model("User", userSchema);
