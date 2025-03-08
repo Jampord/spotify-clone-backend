05277ef43384192e5fdcc0e8b4822334937ee675
John Ford Actub <cs.actubjohnford@gmail.com>
2025-03-08 13:09:39 +0800

    added controller on route

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
+import { getAdmin } from "../controller/admin.controller.js"; // put .js in the end
 
 const router = Router();
 
 // can be router.get or .post  or .put or .patch
-router.get("/", (req, res) => {
-  res.send("Hello from admin route");
-});
+router.get("/", getAdmin);
 
 export default router;
