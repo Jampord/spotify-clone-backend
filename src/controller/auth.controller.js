c63d5198e2237d0fb634f5754f46a152e8836373
Jampord <cs.actubjohnford@gmail.com>
2025-03-17 14:30:38 +0800

    refactor authCallback to use next for error handling and improve code readability

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,24 +1,24 @@
-import { User }from "../models/user.model.js"; // User
+import { User } from "../models/user.model.js"; // User
 
-export const authCallback = async (req, res) => {
-    try {
-      const {id, firstName, lastName, imageUrl} = req.body;
-  
-      //checks if user already exists
-      const user = await User.findOne({ clerkId: id });
-  
-      if(!user) {
-        //sign up user
-     await User.create({
-      clerkId: id,
-      fullName: `${firstName} ${lastName}`,
-      imageUrl: imageUrl,
-     })
-      }
-  
-      res.status(200).json({ message: "User created!", success: true });
-    } catch (error) {
-      console.log("Error in auth callback: ",error);
-      res.status(500).json({ message: "Internal Server Error", error, success: false });
+export const authCallback = async (req, res, next) => {
+  try {
+    const { id, firstName, lastName, imageUrl } = req.body;
+
+    //checks if user already exists
+    const user = await User.findOne({ clerkId: id });
+
+    if (!user) {
+      //sign up user
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl: imageUrl,
+      });
     }
+
+    res.status(200).json({ message: "User created!", success: true });
+  } catch (error) {
+    console.log("Error in auth callback: ", error);
+    next(error);
   }
+};
