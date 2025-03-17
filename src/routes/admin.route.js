41f400703b0fdf89f0beb9f8991a2d61a185d878
Jampord <cs.actubjohnford@gmail.com>
2025-03-17 14:34:06 +0800

    add admin routes with authentication middleware for album and song management

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
-import { getAdmin } from "../controller/admin.controller.js"; // put .js in the end
+import { checkAdmin, createAlbum, createSong, deleteAlbum, deleteSong } from "../controller/admin.controller.js"; // put .js in the end
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-// can be router.get or .post  or .put or .patch
-router.get("/", getAdmin);
+router.use(protectRoute, requireAdmin); //protects all routes and requires admin to routes below it
+
+//-------------- can be router.get or .post  or .put or .patch
+
+//checks if user is admin
+// router.get("/check", protectRoute, requireAdmin, checkAdmin);
+router.get("/check", checkAdmin);
+
+//for songs
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+//for albums
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
