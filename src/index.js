3e95bec0162b883dd6ba86dc5a8a7e0cb348841d
Jampord <cs.actubjohnford@gmail.com>
2025-03-17 14:30:14 +0800

    added clerk middleware for authentication and file upload support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
@@ -8,13 +9,25 @@ import authRoutes from "./routes/auth.route.js";
 import songsRoutes from "./routes/songs.route.js";
 import albumsRoutes from "./routes/albums.route.js";
 import statsRoutes from "./routes/stats.route.js";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // to parse or use req.body (on routes)
+app.use(clerkMiddleware()); // will add auth to req obj. example: req.auth.userId
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+    createParentPath: true,
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+  })
+);
 
 // for routing
 app.use("/api/users", userRoutes);
@@ -23,6 +36,14 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songsRoutes);
 app.use("/api/albums", albumsRoutes);
 app.use("/api/stats", statsRoutes);
+
+//Error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: process.env.NODE_ENV === "production" ? "Internal server error. Something went wrong!" : err.message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
