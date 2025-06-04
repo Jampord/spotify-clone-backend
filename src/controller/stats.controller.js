a5a15830e85c36aff9e33ed3edc911baac5d1b03
Jampord <cs.actubjohnford@gmail.com>
2025-06-04 16:35:39 +0800

    fix: bug on stats controller

--- src/controller/stats.controller.js
+++ src/controller/stats.controller.js
@@ -3,38 +3,35 @@ import { User } from "../models/user.model.js";
 import { Album } from "../models/album.model.js";
 
 export const getStats = async (req, res, next) => {
-  async (req, res, next) => {
-    try {
-      // const totalSongs = await Song.countDocuments();
-      // const totalUsers = await User.countDocuments();
-      // const totalAlbums = await Album.countDocuments();
+  try {
+    // const totalSongs = await Song.countDocuments();
+    // const totalUsers = await User.countDocuments();
+    // const totalAlbums = await Album.countDocuments();
 
-      const [totalSongs, totalUsers, totalAlbums, uniqueArtists] = await Promise.all([
-        Song.countDocuments(),
-        User.countDocuments(),
-        Album.countDocuments(),
+    const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
+      Song.countDocuments(),
+      Album.countDocuments(),
+      User.countDocuments(),
 
-        Song.aggregate([
-          {
-            $unionWith: {
-              coll: "albums",
-              pipeline: [],
-            },
+      Song.aggregate([
+        {
+          $unionWith: {
+            coll: "albums",
+            pipeline: [],
           },
-          {
-            $group: {
-              _id: "$artist",
-            },
+        },
+        {
+          $group: {
+            _id: "$artist",
           },
-          {
-            $count: "count",
-          },
-        ]),
-
-        res.status(200).json({ totalSongs, totalUsers, totalAlbums, totalArtists: uniqueArtists[0].count || 0 }),
-      ]); // runs all the promises in parallel; optimized version of the commented code above
-    } catch (error) {
-      next(error);
-    }
-  };
+        },
+        {
+          $count: "count",
+        },
+      ]),
+    ]); // runs all the promises in parallel; optimized version of the commented code above
+    res.status(200).json({ totalAlbums, totalSongs, totalUsers, totalArtists: uniqueArtists[0].count || 0 });
+  } catch (error) {
+    next(error);
+  }
 };
