afbce01ea6afeb3027f54d3f8e974e410e766618
Jampord <cs.actubjohnford@gmail.com>
2025-06-18 16:53:32 +0800

    fix: correct file upload function and variable naming in admin controller

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -5,7 +5,7 @@ import cloudinary from "../lib/cloudinary.js";
 //helper function for Cloudinary upload
 const uploadToCloudinary = async (file) => {
   try {
-    const result = await cloudinary.uploader(file.tempFilePath, {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
       resource_type: "auto",
     });
     return result.secure_url;
@@ -17,13 +17,13 @@ const uploadToCloudinary = async (file) => {
 
 export const createSong = async (req, res, next) => {
   try {
-    if (!req.files || !req.files.audioFilee || !req.files.imageFile) {
+    if (!req.files || !req.files.audioFile || !req.files.imageFile) {
       return res.status(400).json({ message: "Please upload all files!" });
     }
 
     const { title, artist, albumId, duration } = req.body;
     const audioFile = req.files.audioFile;
-    const imageFile = req.filesss.imageFile;
+    const imageFile = req.files.imageFile;
 
     const imageUrl = await uploadToCloudinary(imageFile);
     const audioUrl = await uploadToCloudinary(audioFile);
@@ -66,7 +66,7 @@ export const deleteSong = async (req, res, next) => {
       });
     }
 
-    await song.findByIdAndDelete(id);
+    await Song.findByIdAndDelete(id);
     res.status(200).json({ message: "Song deleted successfully!" });
   } catch (error) {
     console.log("Error in deleteSong: ", error);
